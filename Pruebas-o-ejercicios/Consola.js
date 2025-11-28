4a1883946bf226bff5a966566e12a00fc23e3443
Jean Keen <keenj942@gmail.com>
2025-11-28 14:17:41 -0400

    a

--- Pruebas-o-ejercicios/Consola.js
+++ Pruebas-o-ejercicios/Consola.js
@@ -1,9 +1,16 @@
-let palabra = prompt('Dime la palabra')
-let K1 = palabra.split(' ')
-let array = [K1]
+try {
+  let palabra = prompt("Dime la palabra");
+  let K1 = palabra.split(" ");
+  let array = K1.length;
+  let arrR = {};
+  let arrT = [];
 
-// for (let index = 0; index < palabra.length; index++) {
-//     const element = array[index];
-    
-// }
-document.writeln(array)
+  // let contador = 0;
+
+  for (let p of K1) {
+    arrR[p] ? arrR[p]++ : (arrR[p] = 1);
+    arrT.push(arrR[p]);
+  }
+} catch (error) {
+  alert("Combo maximo");
+}
