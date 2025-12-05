e4009628a070a1d01f158bcaf1088e8c7a8b8d22
Jean Keen <keenj942@gmail.com>
2025-12-05 11:22:57 -0400

    A

--- Pruebas-o-ejercicios/Pruebas/Consola.js
+++ Pruebas-o-ejercicios/Pruebas/Consola.js
@@ -1,7 +1,1 @@
-let a, b, c;
-
-a = 3;
-b = 2;
-c = `Es igual a ${a + b}`;
-
-document.writeln(c);
+console.log("Hola soy...");
---
+++ Pruebas-o-ejercicios/Pruebas/Variables.js
@@ -0,0 +1,8 @@
+let a = "hola";
+let a1 = 1;
+
+let e, e1;
+e = 10;
+e1 = "soy...";
+
+document.writeln(`${a} ${e1} tengo ${a1} hermano y ${e} hermanas`);
