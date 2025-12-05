5cd41f46a2332ff3ff969efc83d81550a78103a5
Jean Keen <keenj942@gmail.com>
2025-12-05 09:33:02 -0400

    a

--- Pruebas-o-ejercicios/Consola.js
+++ Pruebas-o-ejercicios/Consola.js
@@ -1,14 +1,7 @@
-try {
-  let palabra = prompt("Dime la palabra");
-  let K1 = palabra.split(" ");
-  let array = K1.length;
-  let arrR = {};
-  let arrT = [];
+let a, b, c;
 
-  for (let p of K1) {
-    arrR[p] ? arrR[p]++ : (arrR[p] = 1);
-    arrT.push(arrR[p]);
-  }
-} catch (error) {
-  alert("Combo maximo");
-}
+a = 3;
+b = 2;
+c = `Es igual a ${a + b}`;
+
+document.writeln(c);
