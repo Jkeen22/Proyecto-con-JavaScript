1d6f4486c6c81991388138439051ba36bd2f1817
Jean Keen <keenj942@gmail.com>
2025-12-05 15:21:09 -0400

    A

--- Pruebas-o-ejercicios/Pruebas/Bucles.js
+++ Pruebas-o-ejercicios/Pruebas/Bucles.js
@@ -1,11 +1,37 @@
 let contador;
 contador = 0;
 
+let respuesta;
+//
 for (i = 0; i < 50; i++) {
   contador++;
   document.writeln(contador);
 }
-
-while ((contador = 50)) {
-  alert("Esto es un bucle");
+document.writeln("<p>");
+//
+while (contador < 100) {
+  contador++;
+  document.writeln(contador);
 }
+document.writeln("<p>");
+//
+do {
+  respuesta = prompt("Haz practicado JavaScript? ").toLocaleLowerCase();
+} while (respuesta !== "si");
+
+document.writeln("Esta bien");
+document.writeln("<p>");
+//
+let array;
+
+array = new Array("Pablo", "Juan");
+
+for (const ARR in array) {
+  document.writeln(ARR);
+}
+document.writeln("<p>");
+//
+for (const element of array) {
+  document.writeln(element);
+}
+document.writeln("<p>");
---
+++ Pruebas-o-ejercicios/Pruebas/Funciones.js
@@ -0,0 +1,9 @@
+function f1(a, b) {
+  return a + b;
+}
+document.writeln(n(1, 3));
+//
+let na = function (c, d) {
+  return c * d;
+};
+document.writeln(na(1, 5));
