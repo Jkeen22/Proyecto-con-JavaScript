fafe71f6e97d0345022e72ebbea91ba37c1a8520
Jean Keen <keenj942@gmail.com>
2025-12-05 13:21:39 -0400

    A

---
+++ Pruebas-o-ejercicios/Pruebas/Array.js
@@ -0,0 +1,7 @@
+let arrayNuevo = ["Casa", "Mesa", "Platos"];
+
+document.writeln(arrayNuevo);
+
+let otroArray = new Array(1, 2, 3, 4, 5);
+
+document.writeln(otroArray);
---
+++ Pruebas-o-ejercicios/Pruebas/Bucles.js
@@ -0,0 +1,11 @@
+let contador;
+contador = 0;
+
+for (i = 0; i < 50; i++) {
+  contador++;
+  document.writeln(contador);
+}
+
+while ((contador = 50)) {
+  alert("Esto es un bucle");
+}
