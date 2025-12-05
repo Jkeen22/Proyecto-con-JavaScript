4b146822abc15c1bba2f889a5c9be0ae44093c01
Jean Keen <keenj942@gmail.com>
2025-12-05 15:29:26 -0400

    A

--- Pruebas-o-ejercicios/Pruebas/Funciones.js
+++ Pruebas-o-ejercicios/Pruebas/Funciones.js
@@ -1,9 +1,12 @@
 function f1(a, b) {
   return a + b;
 }
-document.writeln(n(1, 3));
+document.writeln(f1(1, 3));
+document.writeln("<p>");
 //
 let na = function (c, d) {
   return c * d;
 };
 document.writeln(na(1, 5));
+document.writeln("<p>");
+//
