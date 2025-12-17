2bdafe95d4c948f9774ad139bd498651b95846d8
Jean Keen <jkeen@cincinnatus.edu.do>
2025-12-17 15:55:19 -0400

    A

---
+++ Prueba3/Prototipo.js
@@ -0,0 +1,9 @@
+const Samuel ={
+    money : 1000
+}
+
+const Keen = {}
+
+Keen.__proto__ = Samuel
+
+console.log(Keen.money);
---
+++ Prueba3/This.js
@@ -0,0 +1,14 @@
+const Obj = {
+    nombre: "Ana",
+    saludar() {
+        console.log(this.nombre);
+    }
+}
+
+Obj.saludar()
+
+//
+
+function Obj(params) {
+    
+}
